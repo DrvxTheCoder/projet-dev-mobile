088c81973cf18cd441d1f11afdcc664ea91d45e7
Paul Ismael Flan <princeflan741@gmail.com>
2023-10-20 00:45:10 -0100

    Adding project files

---
+++ lib/components/dynamic_form_field_model.dart
@@ -0,0 +1,29 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'dynamic_form_field_widget.dart' show DynamicFormFieldWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class DynamicFormFieldModel extends FlutterFlowModel<DynamicFormFieldWidget> {
+  ///  State fields for stateful widgets in this component.
+
+  // State field(s) for ingredientField widget.
+  FocusNode? ingredientFieldFocusNode;
+  TextEditingController? ingredientFieldController;
+  String? Function(BuildContext, String?)? ingredientFieldControllerValidator;
+
+  /// Initialization and disposal methods.
+
+  void initState(BuildContext context) {}
+
+  void dispose() {
+    ingredientFieldFocusNode?.dispose();
+    ingredientFieldController?.dispose();
+  }
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+}
